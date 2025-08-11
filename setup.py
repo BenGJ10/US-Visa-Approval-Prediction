d7a67b06363a32a6a5b2f3e224b0acf029887c23
BenGJ10 <bengj1015@gmail.com>
2025-08-11 22:17:48 +0530

    Added requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "usvisa",
+    version = "0.0.0",
+    author = "BenGJ",
+    author_email = "bengj1015@gmail.com",
+    packages = find_packages()
+)
