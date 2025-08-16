980d89ca03a120c5700a21760690d22349255c86
BenGJ10 <bengj1015@gmail.com>
2025-08-16 11:04:16 +0530

    Added Data Validation entities

--- usvisa/entity/artifact_entity.py
+++ usvisa/entity/artifact_entity.py
@@ -14,3 +14,17 @@ class DataIngestionArtifact:
     """
     train_file_path: str
     test_file_path: str
+
+@dataclass
+class DataValidationArtifact:
+    """
+    Data class for storing paths related to data validation artifacts.
+    This class holds the file paths for valid and invalid training and testing datasets,
+    as well as the path for the drift report file.
+    """
+    validation_status: bool
+    valid_train_file_path: str
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
+    drift_report_file_path: str
--- usvisa/entity/config_entity.py
+++ usvisa/entity/config_entity.py
@@ -38,6 +38,7 @@ class DataIngestionConfig:
     - Settings like train/test split ratio.
     - The MongoDB collection and database names from constants.
     """
+
     data_ingestion_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_INGESTION_DIR_NAME)
     # File path where the full dataset (feature store) will be saved
     feature_store_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR, FILE_NAME)
@@ -47,3 +48,28 @@ class DataIngestionConfig:
     train_test_split_ratio: float = DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
     # MongoDB configurations
     collection_name:str = DATA_INGESTION_COLLECTION_NAME
+
+@dataclass
+class DataValidationConfig:
+    """
+    Configuration class for the data validation component of the pipeline.
+    This includes:
+    - Directories for valid and invalid data.
+    - File paths for valid and invalid training/testing data.
+    - Path for the drift report file.
+    """
+
+    data_validation_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_VALIDATION_DIR_NAME)
+
+    valid_data_dir: str = os.path.join(data_validation_dir, DATA_VALIDATION_VALID_DIR)
+    invalid_data_dir: str = os.path.join(data_validation_dir, DATA_VALIDATION_INVALID_DIR)
+
+    valid_train_file_path: str = os.path.join(valid_data_dir, TRAIN_FILE_NAME)
+    valid_test_file_path: str = os.path.join(valid_data_dir, TEST_FILE_NAME)
+
+    invalid_train_file_path: str = os.path.join(invalid_data_dir, TRAIN_FILE_NAME)
+    invalid_test_file_path: str = os.path.join(invalid_data_dir, TEST_FILE_NAME)
+
+    drift_report_file_path: str = os.path.join(data_validation_dir, 
+                                               DATA_VALIDATION_DRIFT_REPORT_DIR, 
+                                               DATA_VALIDATION_DRIFT_REPORT_FILE_NAME)
